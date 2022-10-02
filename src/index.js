371657d6c0d30e790ce9e3c743f0379de888503e
Thongphanh Duangboudda <Thongphanh.Duangboudda@contractor.prometric.com>
2022-10-01 23:02:01 -0700

    jj

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ import 'antd/dist/antd.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
     <Provider store={store}>
       <Header></Header>
       <App />
    </Provider>,
  
    
-  </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
